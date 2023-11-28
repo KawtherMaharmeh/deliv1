03fef49fc56b53acbfe0291b5c96f74c112a3d39
khalduon <khalduon@DESKTOP-IV0INRN>
2023-11-28 09:39:39 -0800

    Add project files.

---
+++ deliv1/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace deliv1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmb1 = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cmb1
+            // 
+            this.cmb1.FormattingEnabled = true;
+            this.cmb1.Items.AddRange(new object[] {
+            "Button ",
+            "ComboBox ",
+            "TextBox "});
+            this.cmb1.Location = new System.Drawing.Point(21, 78);
+            this.cmb1.Name = "cmb1";
+            this.cmb1.Size = new System.Drawing.Size(150, 21);
+            this.cmb1.TabIndex = 1;
+            this.cmb1.SelectedIndexChanged += new System.EventHandler(this.cmb1_SelectedIndexChanged);
+            this.cmb1.Click += new System.EventHandler(this.cmb1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnPrint);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtNum);
+            this.panel1.Controls.Add(this.cmb1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(187, 450);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(150)))));
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(187, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(613, 450);
+            this.panel2.TabIndex = 3;
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(21, 141);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(150, 20);
+            this.txtNum.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Controls";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Numbers";
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(36, 221);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(75, 23);
+            this.btnPrint.TabIndex = 5;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "FRM_work";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox cmb1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Button btnPrint;
+    }
+}
+
---
+++ deliv1/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace deliv1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void btn_create_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmb1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void cmb1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            int counter = Convert.ToInt32(txtNum.Text);
+
+
+            String result = cmb1.Text;
+            if (result == "ComboBox ")
+            {
+
+                panel2.Controls.Clear();
+                for (int i = 0; i < counter; i++)
+                {
+
+
+
+                    ComboBox cmb1 = new ComboBox();
+                    cmb1.Name = "KAWTHER" + (i + 1);
+                    cmb1.Text = "kawther" + (i + 1);
+                    cmb1.Location = new Point(25, (i * 50));
+                    cmb1.Size = new Size(100, 40);
+                    panel2.Controls.Add(cmb1);
+
+                }
+            }
+            else if (result == "Button ")
+            {
+                panel2.Controls.Clear();
+
+
+                for (int i = 0; i < counter; i++)
+                {
+                    Button btn1 = new Button();
+
+                    btn1.Text = "kawther" + (i + 1);
+                    btn1.Location = new Point(25, (i * 50));
+                    btn1.Size = new Size(100, 40);
+                    panel2.Controls.Add(btn1);
+
+                }
+            }
+            else if (result == "TextBox ")
+            {
+                panel2.Controls.Clear();
+                for (int i = 0; i < counter; i++)
+                {
+                    TextBox txt1 = new TextBox();
+
+                    txt1.Text = "kawther" + (i + 1);
+                    txt1.Location = new Point(25, (i * 50));
+                    txt1.Size = new Size(100, 40);
+                    panel2.Controls.Add(txt1);
+                }
+            }
+
+        }
+    }
+}
